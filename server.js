c1a22a7d3a87f510c4b5fc013ad3eddd54fe3fa7
chentsu <wox.chentsu@gmail.com>
2016-11-14 17:20:36 +0300

    Обработчик запроса страницы /translate. Функция перевода текста через Яндекс.Переводчик.

--- server.js
+++ server.js
@@ -11,13 +11,18 @@ http.createServer( function (request, response) {
     var params = urlutils.parse('http://localhost:8000'+request.url, true); // кусочки разпарсеного запроса
     // console.log(params);
 
-    if ( params.path === '/news') {
+    if ( params.pathname === '/news') {
         console.log('ща будут новости');
         // передать response в качестве параметра это походу единственный способ дождаться ответа
         // пока response будет мотыляться в другой функции, callback-функция для createServer будет ждать(как я понял)
         // иначе node запустив callback помчится выполнять код дальше, не дожидаясь результатов
         // или node ждёт response.end() ?
         getNews(response);
+    }
+    else if (params.pathname === '/translate')
+    {
+        console.log('посылаем гонца к толмачу');
+        getTranslate(response, params);
     }
     else
     {
@@ -55,7 +60,7 @@ function getNews(responseOnRequestToServer) {
             $('.esc-body').each(function (i, element)
             {
                 news ='<div class="news">';
-                news +='<div class="news-title"><a href="'+$(element).find('.esc-lead-article-title > a').attr('url')+'" >'+ $(element).find('.esc-lead-article-title > a > span').text().trim() + '</a></div>';
+                news +='<div class="news-title"><a href="'+$(element).find('.esc-lead-article-title > a').attr('url')+'" >'+ $(element).find('.esc-lead-article-title > a > span').text().trim() + '</a><button id="btn" type="button" class="trans-btn">Translate</button></div>';
                 news +='<div class="news-text">'+$(element).find('.esc-lead-snippet-wrapper').text().trim() +'</div></div>';
                 responseOnRequestToServer.write(news);
             });
@@ -80,4 +85,32 @@ function getNews(responseOnRequestToServer) {
             console.error('гонца перехватили супостаты!');
         }
     });
+}
+function getTranslate(responseToClient, GETRequestParams) {
+    // console.log(GETRequestParams.query.native);
+        //Если свойство translate свойста query определено, обращаемся к яндекс-переводчику
+     if (GETRequestParams.query.native) {
+        var word = GETRequestParams.query.native;
+         // word=word.replace(/"/g, '\\"');
+         // console.log(word);
+
+         //TODO: разобраться почему в callback в responseBody попадает урезанная версия ответа
+        //Обращаемся к Яндекс.переводчику
+        req.post('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161112T161318Z.6c3a4b81e417ae28.df471210579d8f150163b213710067ffe75860fc&lang=ru-en&format=html&text=' + word, function (error, response, responseBody) {
+            if (!error && response.statusCode === 200) {
+                //Распарсиваем html в объект
+                // console.log(responseBody);
+                var answer = JSON.parse(responseBody);
+                answer = answer.text;
+                answer[0].replace(/\\"/g, '"');
+                // console.log(answer[0]);
+                //Выводим значение свойства text полученного объекта.
+                responseToClient.write(answer[0]);
+                responseToClient.end();
+            }
+            else{
+                console.log('толмач устал, гонца прогнал');
+            }
+        });
+    }
 }
